8b4d9a6391b1d6aedab6f8a52bc74e4ee945d069
nhd <thestraybyte@gmail.com>
2025-10-09 17:54:29 +0530

    feat: start http server and basic error handling

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/nhdfr/workout/internal/app"
+)
+
+func main() {
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+	app.Logger.Println("we are running the app")
+	
+	http.HandleFunc("/health", HealthCheck)
+
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available \n")
 }
